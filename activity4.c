567af568e2f80b5078578bf0b41fa87317884ce7
JP <jpmagday21@gmail.com>
2025-04-06 00:51:56 +0800

    Final

--- activity4.c
+++ activity4.c
@@ -41,7 +41,12 @@ void mainMerge(int arr[], int size){
 void searchLinear(int arr[], int size) {
 	int findValue, pos = -1, i;
 
-	printf("\nEnter value to search: ");
+    printf("\nMerged Array: \n");
+    for(int i = 0; i < size; i++){
+        printf("%d  ", arr[i]);
+    }
+
+	printf("\n\nEnter value to search: ");
 	scanf("%d", &findValue);
 
 	for (int i = 0; i < size; i++) {
@@ -52,9 +57,9 @@ void searchLinear(int arr[], int size) {
 	}
 
 	if (pos == -1) {
-		printf("\nValue not found.\n");
+		printf("\nNot found! %d is not present in the list.\n", findValue);
 	} else {
-		printf("\nValue found at position: %d\n", pos);
+		printf("\n%d can be found at position %d.\n", findValue, pos);
 	}
 
     //sort to get the maximum and minimum values
@@ -70,8 +75,8 @@ void searchLinear(int arr[], int size) {
         }
     }
 
-    printf("\nThe minimum value is: %d", arr[0]);
-    printf("\nThe maximum value is: %d\n", arr[size - 1]);
+    printf("\nThe minimum value found in the array is: %d", arr[0]);
+    printf("\nThe maximum value found in the array is: %d\n", arr[size - 1]);
 }
 
 void searchBinary(int arr[], int size) {
@@ -100,38 +105,38 @@ void searchBinary(int arr[], int size) {
                 printf("%d  ", arr[i]);
             }
 
-            printf("\n");
+            printf("\n\nEnter the value to search: ");
+	        scanf("%d", &search);
+
+            first = 0;
+            last = size - 1;
+
+            while (first <= last) {
+                middle = (first + last) / 2;
+
+                if (arr[middle] < search) {
+                    first = middle + 1;
+                } else if (arr[middle] == search) {
+                    printf("\n%d can be found at index %d.\n", search, middle);
+                    printf("and found at position %d.\n", middle + 1);
+                    return;
+                } else {
+                    last = middle - 1;
+                }
+            }
+
+	        printf("\nNot found! %d is not present in the list.\n", search);
+        } else if (sortConfirm == 'n' || sortConfirm == 'N'){
+            printf("\nUse the linear search instead.\n");
         } else {
-            printf("\nYou can proceed.\n");
+            printf("Invalid Input!");
         }
-
-	printf("\nEnter the value to search: ");
-	scanf("%d", &search);
-
-	first = 0;
-	last = size - 1;
-
-	while (first <= last) {
-		middle = (first + last) / 2;
-
-		if (arr[middle] < search) {
-			first = middle + 1;
-		} else if (arr[middle] == search) {
-			printf("\n%d is present at index %d.\n", search, middle);
-            printf("\n%d is present at position %d in the array.\n", search, middle + 1);
-			return;
-		} else {
-			last = middle - 1;
-		}
-	}
-
-	printf("\nNot found! %d is not present in the list.\n", search);
 }
 
 void searchMain(int arr[], int size) {
 	int pickSearch, pickLinearSearch;
 
-	printf("\nPick the type of search (Linear - 1 / Binary - 2): ");
+	printf("\nPick the type of search (Linear - 1 | Binary - 2): ");
 	scanf("%d", &pickSearch);
 
 	if (pickSearch == 1) {
@@ -145,7 +150,7 @@ void searchMain(int arr[], int size) {
 //End of Search Array Function
 //--------------------------------------------------------
 //This is for Sort Array Function
-void sortBubble(int arr[], int size) {
+void sortBubbleAscend(int arr[], int size) {
     printf("\nBubble Sorted Array:\n");
     for (int i = 0; i < size - 1; i++) {
 
@@ -165,7 +170,27 @@ void sortBubble(int arr[], int size) {
     printf("\n");
 }
 
-void sortSelection(int arr[], int size) {
+void sortBubbleDescend(int arr[], int size) {
+    printf("\nBubble Sorted Array:\n");
+    for (int i = 0; i < size - 1; i++) {
+
+        for (int j = 0; j < size - 1 - i; j++) {
+
+            if (arr[j] < arr[j + 1]) {  // Swap only if needed
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < size; i++) {
+        printf("%d  ", arr[i]);
+    }
+    printf("\n");
+}
+
+void sortSelectionAscend(int arr[], int size) {
     int position;
 
     for (int i = 0; i < size - 1; i++) {
@@ -192,16 +217,61 @@ void sortSelection(int arr[], int size) {
     printf("\n");
 }
 
-void sortMain(int arr[], int size) {
-    int pickSort;
+void sortSelectionDescend(int arr[], int size) {
+    int position;
 
-    printf("\nPick the type of sort (Bubble - 1 / Selection - 2): ");
+    for (int i = 0; i < size - 1; i++) {
+        position = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[position] < arr[j]) {
+                position = j;
+            }
+        }
+
+        if (position != i) {  // Swap only if needed
+            int temp = arr[i];
+            arr[i] = arr[position];
+            arr[position] = temp;
+        }
+    }
+
+    printf("\nSelection Sorted Array:\n");
+
+    for (int i = 0; i < size; i++) {
+        printf("%d  ", arr[i]);
+    }
+    
+    printf("\n");
+}
+
+void sortMain(int arr[], int size) {
+    int pickSort, whatSort;
+
+    printf("\nPick the type of sort (Bubble - 1 | Selection - 2): ");
     scanf("%d", &pickSort);
 
     if (pickSort == 1) {
-        sortBubble(mergeArray, arraySize);
+        printf("\nHow do you wanted it to be sorted? (Ascending - 1 | Descending - 2): ");
+        scanf("%d", &whatSort);
+
+        if (whatSort == 1){
+            sortBubbleAscend(mergeArray, arraySize);
+        } else if (whatSort == 2){
+            sortBubbleDescend(mergeArray, arraySize);
+        } else {
+            printf("\nInvalid Input.\n");
+        }
     } else if (pickSort == 2) {
-        sortSelection(mergeArray, arraySize);
+        printf("\nHow do you wanted it to be sorted? (Ascending - 1 | Descending - 2): ");
+        scanf("%d", &whatSort);
+
+        if (whatSort == 1){
+            sortSelectionAscend(mergeArray, arraySize);
+        } else if (whatSort == 2){
+            sortSelectionDescend(mergeArray, arraySize);
+        } else {
+            printf("\nInvalid Input.\n");
+        }
     } else {
         printf("\nInvalid Input.\n");
     }
@@ -211,39 +281,55 @@ void sortMain(int arr[], int size) {
 //--------------------------------------------------------
 //Main Function
 int main() {
-    int seSo;
-    char tryAgain;
+    int seSo, useOldValues = 0;
+    char tryAgain, keepArr;
 
-        printf("\nEnter values for Array N:\n");
-        for (int i = 0; i < 5; i++) {
-            scanf("%d", &arrayN[i]);
-        }
-
-        printf("\nEnter values for Array B:\n");
-        for (int i = 0; i < 5; i++) {
-            scanf("%d", &arrayB[i]);
-        }
-
-        // Merge arrays before searching/sorting
-        mainMerge(mergeArray, arraySize);
-    
     do {
-        printf("\n\nPick whether to SEARCH or SORT the array (SEARCH - 1 / SORT - 2): ");
+        if (!useOldValues) {
+            printf("\nEnter values for Array N:\n");
+            for (int i = 0; i < 5; i++) {
+                scanf("%d", &arrayN[i]);
+            }
+
+            printf("\nEnter values for Array B:\n");
+            for (int i = 0; i < 5; i++) {
+                scanf("%d", &arrayB[i]);
+            }
+
+            // Merge arrays before searching/sorting
+            mainMerge(mergeArray, arraySize);
+        }
+
+        printf("\n---------------------------------------------------------------------");
+        printf("\nPick whether to SEARCH or SORT the array (SEARCH - 1 | SORT - 2): ");
         scanf("%d", &seSo);
 
         if (seSo == 1) {
-            searchMain(mergeArray, arraySize);  // Assuming searchMain() operates on mainMerge
+            searchMain(mergeArray, arraySize);
         } else if (seSo == 2) {
             sortMain(mergeArray, arraySize);
         } else {
             printf("Invalid Input.");
         }
 
-        printf("\nDo you want to try again? (Y/N): ");
+        printf("\nDo you want to try again?(Y/N): ");
         scanf(" %c", &tryAgain);
-    } while (tryAgain == 'Y' || tryAgain == 'y');
+
+        if (tryAgain == 'Y' || tryAgain == 'y') {
+            printf("Do you want to reuse the existing arrays?(Y/N): ");
+            scanf(" %c", &keepArr);
+
+            if(keepArr == 'Y' || keepArr == 'y'){
+                useOldValues = 1;
+            } else {
+                useOldValues = 0;
+            }   
+        } else {                                                         
+            break;
+        }
+
+    } while (1);
 
     printf("\nThank you for using our program!\n");
-
     return 0;
 }
